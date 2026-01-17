a36131856cadfd95c0b832aaf6d737dba76c55e9
Abbas <abbasmohammadi6@gmail.com>
2026-01-17 20:49:29 +0330

    store debug.log in the root of project

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,7 @@
 import { writeFileSync } from "fs"
 import { join } from "path"
 
-const DEBUG_LOG_PATH = join(import.meta.dir, "..", "debug.log")
+const DEBUG_LOG_PATH = join(import.meta.dir, "../..", "debug.log")
 
 export function logToFile(...messages: unknown[]): void {
   const logEntry = `${messages.map(i => JSON.stringify(i)).join(' ')}\n`
